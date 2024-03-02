bc07bb45275737a6b87354a189bb85f9712f1a12
Korkin Pershutov <korkin.06@inbox.ru>
2024-03-02 14:40:51 +0300

    create file

---
+++ main.php
@@ -0,0 +1,32 @@
+<?php
+
+$string = "2 + 3 * 4 ((((((((((((";
+
+function Calc($string) 
+{
+    
+    $string = str_replace(" ", "", $string);
+
+    $arr = [];
+
+    $SIZE = strlen($string);
+
+    for ($i = 0; $i < $SIZE; $i++) {
+        
+        switch($string[$i]) {
+            case "+":
+
+        }
+            
+
+        array_push($arr, $string[$i]);
+    }
+
+
+
+    return print_r($arr);
+}
+
+echo Calc($string);
+
+print_r(count_chars($string, 1)[ord("(")]);
